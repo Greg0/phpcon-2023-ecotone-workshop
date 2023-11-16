3717ba87c5213b3206879021430daec281eb2e62
Grzegorz Kuźnik <gerg0sz92@gmail.com>
2023-11-16 12:13:29 +0100

    Task 2. Place order

--- 2-building-blocks/src/Domain/Order/Order.php
+++ 2-building-blocks/src/Domain/Order/Order.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Domain\Order;
 
+use App\Application\Order\PlaceOrder;
 use App\Domain\Order\Event\OrderWasCancelled;
+use App\Domain\Order\Event\OrderWasPlaced;
 use Doctrine\ORM\Mapping as ORM;
 use Ecotone\Modelling\Attribute\Aggregate;
 use Ecotone\Modelling\Attribute\CommandHandler;
@@ -34,11 +36,13 @@ final class Order
         $this->orderId = $orderId;
         $this->productId = $productId;
         $this->isCancelled = false;
+        $this->recordThat(new OrderWasPlaced($this->orderId, $this->productId));
     }
 
-    public static function create(string $orderId, string $productId): self
+    #[CommandHandler]
+    public static function create(PlaceOrder $cmd): self
     {
-        return new self($orderId, $productId);
+        return new self($cmd->orderId, $cmd->productId);
     }
 
     #[CommandHandler(routingKey: 'order.cancel')]
